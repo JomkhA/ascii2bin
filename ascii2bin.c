558998ef2a893ced1c54ad100cc74f48184f8ef9
Akkharaw Jomkhanthiphol <akkharaw.jomkhanthiphol.179@my.csun.edu>
2021-02-28 15:01:37 -0800

    FINISHED

--- ascii2bin.c
+++ ascii2bin.c
@@ -13,63 +13,38 @@
 #include <unistd.h>
 #include <math.h>
 
-#define maxint (32)
-
 int main (int argc, char * argv[], char ** envp) {
 
-  char string[maxint];
-  read(0, &string, maxint);
-  printf("%s", string); // CHECK IF IT MATCHES BINARY STRING
-  int length = 0;
+  int ascii_value;
+  int offset = 48;
+  int number = 0;
+  int count = 0;
 
-  for (int x = 0; x < maxint; x++) {
-    if (string[x] == '0' || string[x] == '1') {
-      length++;
+  int retval = read(0, &ascii_value, 1);
+
+  while (retval == 1) {
+    count++;
+
+    if (count > 32) {
+      fprintf(stderr, "%s", "Error, number exceeds 2^32!");
+      return 1;
     }
-    else {
+
+    if (ascii_value != '0' && ascii_value != '1') {
+      fprintf(stderr, "%s", "Error, invalid input!");
+      return 1;
+    }
+
+    int digit = ascii_value - offset;
+    number = (number << 1) + digit;  
+    retval = read(0, &ascii_value, 1);
+
+    if (ascii_value == 10 || ascii_value == 13) {
       break;
     }
   }
 
-  printf("%d\n", length); // CHECK LENGTH
-
-  int number;
-  int index = length - 1;
-
-  for (int x = 0; x < length; x++) {
-    if (string[x] = '1') {
-      number = number + pow(2, index);
-      index--;
-    }
-    if (string[x] = '0') {
-      index--;
-    }
-  }
-
-  printf("Number = %d", number);
-
-
-  //char string[10];
-  //int strlength = strlen(string);
-  //printf("Length = %d" + strlength);
-  //printf("String:" + (char) * string);
-
-  //int ascii_value;
-  //int offset = 48;
-  //int number = 0;
-  //int retval = read(0, &ascii_value, 1);
-
-  //while (retval == 1) {
-      //int digit = ascii_value - offset;
-      //number = number << 1 + digit;  
-      //retval = read(0, &ascii_value, 1);
-  //}
-        
-  //printf("%d\n", number);
-
-  //if (checksum != complement) {
-    //fprintf(stderr, "Error Detected!\n"); 
-    //return 1;
-  //}
-  //return 0;
+  printf("Integer: ");
+  printf("%lld\n", number);
+  return 0;
 }
